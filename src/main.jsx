0c8d4725a32c9de50e7905bb7c75d474a532d9a8
Zeq <106398287+Z-eq@users.noreply.github.com>
2026-02-21 19:57:25 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CiscoLabSimulator from './CiscoLabSimulator'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CiscoLabSimulator />
+  </React.StrictMode>
+)
